9ad0ee62e8f5d72871bc91a168abf21d9ab6a15a
jrandet <jrandet@student.42.fr>
2025-09-17 22:42:55 +0200

    understood dictionary

---
+++ disctionary_data_base.py
@@ -0,0 +1,17 @@
+customer = {
+	"name": "SwissTech AG",
+	"health_score": 8.5,
+	"monthly_logins": 25,
+	"support_tickets": 1,
+	"mrr": 2500.00
+}
+
+"""
+this is a coment
+"""
+
+print(f"Company: {customer['name']}")
+print(f"Health score: {customer['health_score']}")
+print(f"Monthly logins: {customer['monthly_logins']}")
+print(f"Support tickets: {customer['support_tickets']}")
+print(f"mrr: {customer['mrr']}")
